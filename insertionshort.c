00807bb88467504a936b6d26759045caede59bd1
0310AJ2422 <130687638+0310AJ2422@users.noreply.github.com>
2023-04-15 22:41:24 +0530

    Add files via upload

---
+++ insertionshort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void insert(int a[],int n)
+{
+    int i,j,temp;
+    for(i=1;i<n;i++)
+    {
+        temp=a[i];
+        j=i-1;
+        while(j>=0&& temp<=a[j])
+        {
+            a[j+1]=a[j];
+            j=j-1;
+        }
+        a[j+1]=temp;
+    }
+}
+void printarr(int a[],int n)
+
+{
+    int i;
+    for(i=0;i<n;i++)
+        printf("%d\th",a[i]);
+}
+int main()
+{
+    int a[]={12,31,25,8,32,17};
+    int n=sizeof(a)/sizeof(a[0]);
+    printf("before insertion\n");
+    printarr(a,n);
+    insert(a,n);
+    printf("\n after sorting arraay elements are-\n");
+    printarr(a,n);
+    return 0;
+}
